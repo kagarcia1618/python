d1aefb06063172d35c2ff0cdf83629fe89063943
Kenkun <kenneth.acar.garcia@gmail.com>
2018-02-23 19:33:20 +0800

    Create netconf_capabilities.py

---
+++ netconf_capabilities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import logging
+from ncclient import manager
+import sys
+
+router = {"ip": "10.30.26.227", 
+          "port": 22, 
+          "user": "zibmkenneth", 
+"pass": "Nuh$@1bm022018"}
+
+def main():
+ 	"""Main method that prints NETCONF capabilities of remote device."""
+
+	m = manager.connect(host=router["ip"], port=router["port"], username=router["user"], password=router["pass"], hostkey_verify=False)
+
+        print('***Here are the Remote Devices Capabilities***')
+        for capability in m.server_capabilities:
+		print(capability)
+
+if __name__ == '__main__':
+	sys.exit(main())
