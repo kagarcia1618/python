c7ef265c1a104be890bc9c7520ff69aae417e6d6
Kenneth <kenneth.acar.garcia@gmail.com>
2018-02-26 09:49:02 +0800

    int_to_str_dict_index

--- parse_cisco_ios_int_desc.py
+++ parse_cisco_ios_int_desc.py
@@ -3,6 +3,8 @@
 from collections import OrderedDict
 from netmiko import ConnectHandler
 from parser_module import parse_cisco_ios_int_desc
+from pprint import pprint
+import xmltodict
 
 def extract(filename):
 	fi = open(filename,'r') #Opens the file
@@ -18,13 +20,13 @@ node_db_raw = extract(node_db_filename)
 node_login_raw = extract(node_login_filename)
 
 for i in range(len(node_db_raw)):
-	node_db[i] = node_db_raw[i].split(',')
-	for j in range(len(node_db[i])):
-		if j == (len(node_db[i])-1):
-			node_db[i][j] = node_db[i][j].strip('\r\n' or '\n')
+	node_db[str(i)] = node_db_raw[i].split(',')
+	for j in range(len(node_db[str(i)])):
+		if j == (len(node_db[str(i)])-1):
+			node_db[str(i)][j] = node_db[str(i)][j].strip('\r\n' or '\n')
 
 for i in range(len(node_login_raw)):
-	node_login[i] = node_login_raw[i].split(',')
+	node_login[str(i)] = node_login_raw[i].split(',')
 
 device_params = OrderedDict()
 output = OrderedDict()
@@ -32,24 +34,26 @@ parsed_data = OrderedDict()
 netmiko_params = ['device_type','ip','username','password']
 
 for i in range(len(node_db)):
-	device_params[i] = OrderedDict()
+	device_params[str(i)] = OrderedDict()
 	for j in netmiko_params:
 		if j == 'device_type':
-			device_params[i][j] = node_db[i][2]
+			device_params[str(i)][str(j)] = node_db[str(i)][2]
 		if j == 'ip':
-			device_params[i][j] = node_db[i][1]
+			device_params[str(i)][str(j)] = node_db[str(i)][1]
 		if j == 'username':
-			device_params[i][j] = node_login[0][0]
+			device_params[str(i)][str(j)] = node_login['0'][0]
 		if j == 'password':
-			device_params[i][j] = node_login[0][1]
+			device_params[str(i)][str(j)] = node_login['0'][1]
 
 for i in range(len(device_params)):
-	net_connect = ConnectHandler(**device_params[i])
-	output[i] = net_connect.send_command("show int desc")
-	parsed_data[i] = parse_cisco_ios_int_desc(output[i])
-	
+	net_connect = ConnectHandler(**device_params[str(i)])
+	output[str(i)] = net_connect.send_command("show int desc")
+	parsed_data[str(i)] = parse_cisco_ios_int_desc(output[str(i)])
+
 for i in range(len(parsed_data)):
-	print('Device ' + node_db[i][0] + ' has the following interface description :\n')
-	for j in range(len(parsed_data[i])):
-		print('\tInterface : ' + parsed_data[i][j][0] + '\n\tDescription : ' +  parsed_data[i][j][3] + '\n')
+	print('Device ' + node_db[str(i)][0] + ' has the following interface description :\n')
+	for j in range(len(parsed_data[str(i)])):
+		print('\tInterface : ' + parsed_data[str(i)][str(j)]['0'] + '\n\tDescription : ' +  parsed_data[str(i)][str(j)]['3'] + '\n')
 	print('')
+
+pprint(str(xmltodict.unparse({'Cisco':parsed_data})))
